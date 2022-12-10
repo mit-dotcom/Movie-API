4bbe6c756da5774f34756a2cc206d49db877ead4
mit-dotcom <54674265+mit-dotcom@users.noreply.github.com>
2022-12-10 14:42:35 +0530

    Delete

--- Movie-master/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use('/movie', require('./movie'));
-router.use('/user', require('./user'));
-
-
-module.exports = router;
